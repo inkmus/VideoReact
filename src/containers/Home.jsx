413818de80e15e94d0e2fc5fdf865c3090393283
kmilo <elkmilo7@gmail.com>
2021-09-07 16:17:40 -0500

    Agregando-los-enlaces-a-mis-paginas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Layout from '../components/Layout'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Carousel from '../components/Carousel'
@@ -14,8 +13,8 @@ const API = 'http://localhost:3000/initalState'
 const Home = () => {
   const initialState = useChannelMovie(API)
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <Layout>
-      <Header />
+    <>
+    
       <Search />
       {initialState.mylist.length > 0 && (
         <Categories title="Recientes">
@@ -40,8 +39,8 @@ const Home = () => {
           )}
         </Carousel>
       </Categories>
-      <Footer />
-    </Layout>
+   
+    </>
   )
 }
 
